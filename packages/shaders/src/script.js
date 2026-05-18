15e5f8cd09210cf431900aa5c75f3e0a42c90282
subikstha <subsatch@gmail.com>
2026-05-18 18:29:29 +0545

    feat: added aRandom attribute and uFrequency and uTime uniforms

--- packages/shaders/src/script.js
+++ packages/shaders/src/script.js
@@ -29,15 +29,31 @@ const textureLoader = new THREE.TextureLoader()
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
+const count = geometry.attributes.position.count
+const randoms = new Float32Array(count)
+
+for(let i = 0; i < count; i++) {
+    randoms[i] = Math.random()
+}
+
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms,1))
+
 // Material
 const material = new THREE.RawShaderMaterial({
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
-    // wireframe: true
-    side: THREE
+    // wireframe: true,
+    // transparent: true,
+    side: THREE.DoubleSide,
+    uniforms: {
+        uFrequency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 }
+    }
 })
 
-// Mesh
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(.01).name('frequencyY')
+// Meshgui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(.01).name('frequencyY')
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -93,7 +109,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+    // Update material
+    material.uniforms.uTime.value = elapsedTime
     // Update controls
     controls.update()
 
--- packages/shaders/src/shaders/test/vertex.glsl
+++ packages/shaders/src/shaders/test/vertex.glsl
@@ -1,16 +1,24 @@
 uniform mat4 projectionMatrix; // will transform the coordinates into the clip space coordinates
 uniform mat4 viewMatrix; // applies transformation relative to the camera(position, rotation, field of view, near, far)
 uniform mat4 modelMatrix; // modelmatrix applies transformation relative to the Mesh(position, rotation, scale)
-
+uniform vec2 uFrequency;
+uniform float uTime;
 // This is the value that was provided in the BufferGeometry in JS
 attribute vec3 position; // It is the data that changes with each vertex
+attribute float aRandom;
+
+// varying float vRandom;
 
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0) * .1;
+    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * .1;
+    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * .1;
+    // modelPosition.z += aRandom * 0.1;
     vec4 viewPosition = viewMatrix * modelPosition;
     // viewPosition.x -= 2.0;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
+
+    // vRandom = aRandom;
 }
