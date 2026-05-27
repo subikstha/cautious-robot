d395a0ee25f105f16cf7a5cc4c2a0aa7477028d5
subikstha <subsatch@gmail.com>
2026-05-27 21:20:15 +0545

    feat: added colored version

--- packages/shader-patterns/src/shaders/test/fragment.glsl
+++ packages/shader-patterns/src/shaders/test/fragment.glsl
@@ -292,7 +292,13 @@ void main()
     // float strength = sin(cnoise(vUv * 10.0) * 20.0);
 
     // Pattern 50
+    // float strength = step(0.5, sin(cnoise(vUv * 10.0) * 20.0));
+
+    // Colored version
     float strength = step(0.5, sin(cnoise(vUv * 10.0) * 20.0));
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    gl_FragColor = vec4(mixedColor, 1.0);
 
 }
