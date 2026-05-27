964653284acdc5608deb49e6d2d37c553eb9a3e1
subikstha <subsatch@gmail.com>
2026-05-27 17:16:44 +0545

    feat: added upto pattern 39

--- packages/shader-patterns/src/shaders/test/fragment.glsl
+++ packages/shader-patterns/src/shaders/test/fragment.glsl
@@ -176,7 +176,28 @@ void main()
     // float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - .25));
 
     // Pattern 36
-    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - .24));
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - .24));
+
+    // Pattern 37
+    // vec2 wavedUv = vec2(
+    //     vUv.x,
+    //     vUv.y + sin(vUv.x * 30.0) * .1
+    // );
+    // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - .24));
+
+    // Pattern 38
+    // vec2 wavedUv = vec2(
+    //     vUv.x + sin(vUv.y * 30.0) * .1,
+    //     vUv.y + sin(vUv.x * 30.0) * .1
+    // );
+    // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - .25));
+
+    // Pattern 39
+    vec2 wavedUv = vec2(
+        vUv.x + sin(vUv.y * 100.0) * .1,
+        vUv.y + sin(vUv.x * 100.0) * .1
+    );
+    float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - .25));
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 
 }
